b22a109f30b63d04bf4515c9f33f79bdcd502ad2
MINYUKYUNG <minew0223@gmail.com>
2022-09-18 21:39:33 +0900

    🎨 Modify : axios 비동기 처리를 redux 에서 처리하는 방식으로 수정

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -4,65 +4,45 @@ const ORDER = '_order=desc';
 const SORT = '_sort=id';
 
 const apiRoot = axios.create({
-  baseURL: 'http://localhost:4000/comments',
+  baseURL: process.env.REACT_APP_API_URL,
   headers: { 'Content-Type': 'application/json' },
 });
 
 const getAllCommentsApi = async () => {
-  try {
-    const { data } = await apiRoot.get(`?${ORDER}&${SORT}`);
-    return data;
-  } catch (error) {
-    throw new Error('not found page');
-  }
+  const { data } = await apiRoot.get(`?${ORDER}&${SORT}`);
+  return data;
 };
 
 const getCommentApi = async (commentId) => {
-  try {
-    const { data } = await apiRoot.get(`/${commentId}`);
-    return data;
-  } catch (error) {
-    throw new Error('not found page');
-  }
+  const { data } = await apiRoot.get(`/${commentId}`);
+  return data;
 };
 
 const postCommentApi = async ({ profile_url, author, content, createdAt }) => {
-  try {
-    const { data } = await apiRoot.post('', {
-      profile_url, 
-      author, 
-      content, 
-      createdAt
-    });
+  const { data } = await apiRoot.post('', {
+    profile_url, 
+    author, 
+    content, 
+    createdAt
+  });
 
-    return data;
-  } catch (error) {
-    throw new Error('not found page');
-  }
+  return data;
 };
 
 const putCommentApi = async (commentId, { profile_url, author, content, createdAt }) => {
-  try {
-    const { data } = await apiRoot.put(`/${commentId}`, {
-      profile_url, 
-      author, 
-      content, 
-      createdAt
-    });
-    
-    return data;
-  } catch (error) {
-    throw new Error('not found page');
-  }
+  const { data } = await apiRoot.put(`/${commentId}`, {
+    profile_url, 
+    author, 
+    content, 
+    createdAt
+  });
+  
+  return data;
 };
 
 const deleteCommentApi = async (commentId) => {
-  try {
-    const { data } = await apiRoot.delete(`/${commentId}`);
-    return data;
-  } catch (error) {
-    throw new Error('not found page');
-  }
+  const { data } = await apiRoot.delete(`/${commentId}`);
+  return data;
 };
 
 export { getAllCommentsApi, getCommentApi, postCommentApi, putCommentApi, deleteCommentApi };

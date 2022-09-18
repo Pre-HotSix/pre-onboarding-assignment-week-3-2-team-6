b6c936db4496ba53094ade019abd52997ed2f9e3
MINYUKYUNG <minew0223@gmail.com>
2022-09-18 23:03:33 +0900

    ⚡️ Feature : 댓글 관리 페이지 재적용

---
+++ src/components/PageList/index.jsx
@@ -0,0 +1,21 @@
+import * as S from './style';
+import { useSelector, useDispatch } from "react-redux";
+import { paginationAction, LIMIT } from '../../redux/modules/pagination';
+
+function PageList() {
+  const pageArray = [];
+  const dispatch = useDispatch();
+  const comments = useSelector(state => state.comments.data);
+  const changePage = (i) => dispatch(paginationAction(i));
+
+  for (let i = 1; i <= Math.ceil(comments.length / LIMIT); i++) {
+    pageArray.push(
+      <S.PageButton key={i} onClick={() => changePage(i)}>{i}</S.PageButton>
+    );
+  };
+
+
+  return <S.PageListStyle>{pageArray}</S.PageListStyle>;
+}
+
+export default PageList;

e18c69fa9372b8100aa6ffcf308a10d9912e1fd4
MINYUKYUNG <minew0223@gmail.com>
2022-09-18 23:18:03 +0900

    💄 Style : pagination Active 버튼 스타일 추가

--- src/components/PageList/index.jsx
+++ src/components/PageList/index.jsx
@@ -7,10 +7,11 @@ function PageList() {
   const dispatch = useDispatch();
   const comments = useSelector(state => state.comments.data);
   const changePage = (i) => dispatch(paginationAction(i));
+  const pagination = useSelector((state) => state.pagination);
 
   for (let i = 1; i <= Math.ceil(comments.length / LIMIT); i++) {
     pageArray.push(
-      <S.PageButton key={i} onClick={() => changePage(i)}>{i}</S.PageButton>
+      <S.PageButton active={i === pagination} key={i} onClick={() => changePage(i)}>{i}</S.PageButton>
     );
   };
 

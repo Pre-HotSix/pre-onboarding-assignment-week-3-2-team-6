139297fb49c867ea53f4ae81a1b17f4731fbca0d
MINYUKYUNG <minew0223@gmail.com>
2022-09-18 17:56:00 +0900

    ⚡️ Feature :  redux 사용을 위한 rootReducer 추가

--- src/redux/modules/index.js
+++ src/redux/modules/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+<<<<<<< HEAD
 import commentsReducer from './comments';
 
 const rootReducer = combineReducers({
@@ -6,3 +7,16 @@ const rootReducer = combineReducers({
 });
 
 export default rootReducer;
+=======
+import { commentsReducer } from './comments';
+import { singleCommentReducer } from './singleComment';
+import { paginationReducer } from './pagination';
+
+const rootReducer = combineReducers({
+  comments: commentsReducer,
+  singleComment: singleCommentReducer,
+  pagination: paginationReducer
+})
+
+export default rootReducer;
+>>>>>>> 3036ef0 (⚡️ Feature :  redux 사용을 위한 rootReducer 추가)
